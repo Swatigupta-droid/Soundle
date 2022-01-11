622e916bc6c1ace70c3ed71503e48128cfbeeb77
Swati Gupta <guptaswati7549@gmail.com>
2022-01-11 20:18:37 +0530

    home page

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express                 = require("express"),
     mongoose                = require("mongoose"),
     passport                = require("passport"),
     bodyParser              = require("body-parser"),
-    User                    = require("./models/user"),
     LocalStrategy           = require("passport-local"),
     passportLocalMongoose   = require("passport-local-mongoose")
     
